57aa6d8c0e28097b6f8267bde9ad60cdcb5f689d
Emily.Garcia <emilikimberly1@gmail.com>
2021-09-24 21:07:06 -0400

    Created InViewportContext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import './App.css';
 import dance from './img/dance.png';
 import cry from './img/cry.png';
@@ -8,18 +8,21 @@ import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Message from './components/Message/Message';
 import Options from './components/Options/Options';
+import { InViewportContext } from './components/InViewportContext/InViewportContext';
 
 
 function App() {
+  const {inViewport} = useContext(InViewportContext);
+  console.log(inViewport);
   return (
     <div className="App karaoke_bgr">
-      <Header />
-      <Message img={dance} label="Dance" img-alt="People dancing"/>
-      <Message img={cry} label="Cry" img-alt="Sunset with guitars"/>
-      <Message img={mariachi} label="Remember" img-alt="Mariachi singing"/>
-      <Message img={concert} label="Feel and sing along" imgAlt="Concert"/>
-      <Options date="Friday, September 24th"/>
-      <Footer />
+        <Header />
+        <Message img={dance} label="Dance" img-alt="People dancing" imgShow={inViewport}/>
+        <Message img={cry} label="Cry" img-alt="Sunset with guitars" imgShow={inViewport}/>
+        <Message img={mariachi} label="Remember" img-alt="Mariachi singing" imgShow={inViewport}/>
+        <Message img={concert} label="Feel and sing along" imgAlt="Concert" imgShow={inViewport}/>
+        <Options date="Friday, September 24th"/>
+        <Footer />
     </div>
   );
 }
--- src/components/Header/Event/Event.jsx
+++ src/components/Header/Event/Event.jsx
@@ -3,7 +3,7 @@ import './Event.css';
 import music from '../../../img/music-transparent-bgr.png'
 
 function Event(props){
-    return <div className="event-div d-flex flex-column align-items-center justify-content-center event-bgr">
+    return <div className="event-div d-flex flex-column align-items-center justify-content-center">
         <h1 className="event-name py-5 ">{props.name}<img src={music} className="img-fluid event-img" alt="Music notes"></img></h1>
     </div>
 }
