5984dd69654ca0baa11ea83bf165fccb1d96c773
Emily.Garcia <emilikimberly1@gmail.com>
2021-10-22 23:27:37 -0400

    Reduced the number of component renders by adding an array of information

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import './App.css';
 import dance from './img/dance.png';
 import cry from './img/cry.png';
@@ -8,19 +8,53 @@ import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Message from './components/Message/Message';
 import Options from './components/Options/Options';
-import { InViewportContext } from './components/InViewportContext/InViewportContext';
 
+function useOnScreen(options){
+  const [ref, setRef] = useState(null);
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setVisible(entry.isIntersecting);
+    }, options);
+
+    if (ref) {
+      observer.observe(ref);
+    }
+
+    return ( () => {
+      if (ref) {
+        observer.unobserve(ref);
+      }
+    })
+  }, [ref, options]);
+  console.log(visible);
+
+  return [setRef, visible];
+}
 
 function App() {
-  const {inViewport} = useContext(InViewportContext);
-  console.log(inViewport);
+  const [setRef, visible] = useOnScreen({ threshold: 0.2 });
+  const [index, setIndex] = useState(0);
+  const imgArray = [{img: dance, label:"Dance", imgAlt:"People dancing"}, 
+                    {img: cry, label:"Cry", imgAlt:"Sunset with guitars"}, 
+                    {img: mariachi, label:"Remember", imgAlt:"Mariachi singing"}, 
+                    {img: concert, label:"Sing along", imgAlt:"Concert"}];
+
+  function handleClick(){
+    if (index === imgArray.length - 1){
+      setIndex(0)
+    } else {
+      setIndex(index + 1)
+    }
+  }
+  
   return (
     <div className="App karaoke_bgr">
         <Header />
-        <Message img={dance} label="Dance" img-alt="People dancing" imgShow={inViewport}/>
-        <Message img={cry} label="Cry" img-alt="Sunset with guitars" imgShow={inViewport}/>
-        <Message img={mariachi} label="Remember" img-alt="Mariachi singing" imgShow={inViewport}/>
-        <Message img={concert} label="Feel and sing along" imgAlt="Concert" imgShow={inViewport}/>
+        <div ref={setRef} onClick={()=>handleClick()}>
+            <Message visible={visible} img={imgArray[index].img} label={imgArray[index].label} imgAlt={imgArray[index].imgAlt}/>
+          </div>
         <Options date="Friday, September 24th"/>
         <Footer />
     </div>
--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,12 +1,23 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './Message.css';
-function Message(props){
-    
-    return <div className="msg-div-index msg-div-height my-5">
-        <h3 className="text-start fs-5 m-0 px-2">Make us</h3>
-        <h2 className="msg-margin msg-lbl">{props.label}</h2>
-        <img src={props.img} className={"img-fluid msg-img-index msg-img-opacity msg-img-border show-on-scroll inline-photo " + (props.imgShow && "is-visible")} alt={props.imgAlt}></img>
-        
-    </div>
+class Message extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+    componentDidUpdate(prevProps) {
+        if (prevProps.img !== this.props.img) {
+          this.setState();
+        }
+    }
+    //Fade-in and fade-out images. Whatch youtube 6min video.
+    render() {
+        return (
+            <div className="msg-div-index msg-div-height my-5">
+                <h3 className="text-start fs-5 m-0 px-2">Make us</h3>
+                <h2 className="msg-margin msg-lbl">{this.props.label}</h2>
+                <img src={this.props.img} className={"img-fluid msg-img-index msg-img-opacity msg-img-border inline-photo" + (this.props.visible ? " is-visible" : "")} alt={this.props.imgAlt}></img>
+            </div>
+        )
+    }
 }
 export default Message
